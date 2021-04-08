ad44c0006289d09d45f2e0b5bb5b8e22396b79e7
antirex <55479324+antirex@users.noreply.github.com>
2021-04-08 10:44:48 +0530

    feat: random templating using react

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Dashboard from './Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../images/overlay.svg'
+
+function App() {
+  return (
+    <div className="container">
+      <img src={logo} />
+      <h1>Foodine</h1>
+      
+      
+    </div>
+  );
+}
+
+export default App;
